6b3fcb47ea67055843512c7910a543830b7d7090
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2022-03-03 09:55:19 -0300

    Update basico.js

--- basico.js
+++ basico.js
@@ -15,3 +15,46 @@ console.log(myFunction()); // declarado variável myVariable dentro da função 
 console.log(myOtherVariable);// Exibira global porque foi referenciado a variável global chamada myOtherVariable inicializada na linha 2
 console.log(myOtherFunction()); // exibira local. Na função myOtherFunction referenciamos a variavél global myOtherVariable e atribuimos o valor local, pois não declaramos a variável usando a palavra reservada var
 console.log(myOtherVariable);//exibira local pois o valor da variavel foi alterado em myOtherFunction.
+
+// Operadores aritméticos.
+
+var num = 0; 
+num = num + 2;
+num = num * 3;
+num = num /2;
+num++;
+num--;
+
+// Operadores de atribuição.
+
+num += 1;
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+
+// Operadores de comparação.
+
+console.log('num == 1 : ' + (num == 1));
+console.log('num === 1 : ' + (num === 1));
+console.log('num != 1 : ' + (num != 1));
+console.log('num > 1 ; ' + (num > 1));
+console.log('num < 1 : ' + (num < 1));
+console.log('num >= 1 : ' + (num >= 1));
+console.log('num <= 1 : ' + (num <= 1));
+
+// Operadores lógicos.
+
+console.log('true && false : ' + (true && false));
+console.log('true || false : ' + (true || false));
+console.log('!true : ' + (!true));
+
+// Operadores Bit a Bit (bitwise)
+
+console.log('5 & 1 : ', (5 & 1));
+console.log('5 | 1 : ', (5 | 1));
+console.log('~ 5 : ', (~5));
+console.log('5 ^ 1 : ', (5 ^ 1));
+console.log('5 << 1 : ', (5 << 1));
+console.log('5 >> 1 : ', (5 >> 1));
+
