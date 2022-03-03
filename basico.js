68d6b2ddc80550a19d79ab21c92c58f8023bddca
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2022-03-03 11:47:07 -0300

    Update basico.js

--- basico.js
+++ basico.js
@@ -58,3 +58,10 @@ console.log('5 ^ 1 : ', (5 ^ 1));
 console.log('5 << 1 : ', (5 << 1));
 console.log('5 >> 1 : ', (5 >> 1));
 
+// Operador typeof
+
+console.log('typeof num: ', typeof num);
+console.log('typeof Packt: ', typeof 'Packt');
+console.log('typeof true: ', typeof true);
+console.log('typeof [1, 2, 3]: ', typeof [1, 2, 3]);
+console.log('typeof {name: John}: ', typeof {name: 'John'});
