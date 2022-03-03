2e6cdc659ace2b52abd09799bb54c0ed4b240530
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2022-03-03 07:03:33 -0300

    Update operadores.js

--- operadores.js
+++ operadores.js
@@ -1,1 +1,16 @@
+// Operadores aritméticos.
+
 var num = 0; 
+num = num + 2;
+num = num * 3;
+num = num /2;
+num++;
+num--;
+
+// Operadores de atribuição.
+
+num += 1;
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
